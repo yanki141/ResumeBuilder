63feab9eaecd4474f0f0c423465926d9aea8cbc2
syanki <syanburnsb@gmail.com>
2021-11-09 17:41:41 +0530

    MVT done PDF pending

--- pdf/models.py
+++ pdf/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+# After creating this, do makemigrations to create table in the framework
+# to create in db we need to migrate
+# To see the table, create superuser: syanki/Syanki@141
+# Can't see this table in Admin interface, include this class in admin.py
+# after registering this model, we can create any Profile from admin interface as well like form filling.
+
+
+class Profile(models.Model):
+    name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    school = models.CharField(max_length=100)
+    degree = models.CharField(max_length=100)
+    university = models.CharField(max_length=100)
+    skill = models.TextField(max_length=100)
+    ambition = models.TextField(max_length=100)
+    work_ex = models.TextField(max_length=100)
--- pdf/views.py
+++ pdf/views.py
@@ -1,3 +1,29 @@
+
+
 from django.shortcuts import render
+from .models import Profile
 
 # Create your views here.
+# Till now we have just created template but not storing any data from the template form.
+# To store the data from form, we need to instruct
+# Get the info from the User by if tag and paste in the db --> name = request.POST.get("name", ""). Also check label
+# In Python, we create object to push the data into db
+
+
+def accept(request):
+    if request.method == "POST":
+        name = request.POST.get("name", "")
+        phone = request.POST.get("phone", "")
+        email = request.POST.get("email", "")
+        degree = request.POST.get("degree", "")
+        university = request.POST.get("university", "")
+        skill = request.POST.get("skill", "")
+        ambition = request.POST.get("ambition", "")
+        work_ex = request.POST.get("work_exp", "")
+        school = request.POST.get("school", "")
+
+        profile = Profile(name=name, phone=phone, email=email, degree=degree, university=university, skill=skill,
+                          ambition=ambition, work_ex=work_ex, school=school)
+        profile.save()
+
+    return render(request, "pdf/accept.html")
